3a50a2663fcc0fd9498ba13e2cff5c811575f440
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-08-16 00:15:37 +0600

    first commit

---
+++ src/firebase.init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB3gT3Vbina_b0kAHSrAT2SsPVmvCHXaz8",
+  authDomain: "bmrc-chat.firebaseapp.com",
+  projectId: "bmrc-chat",
+  storageBucket: "bmrc-chat.appspot.com",
+  messagingSenderId: "356624821473",
+  appId: "1:356624821473:web:5c145a55f20a47f5691ba0"
+};
+
+const app = initializeApp(firebaseConfig);
