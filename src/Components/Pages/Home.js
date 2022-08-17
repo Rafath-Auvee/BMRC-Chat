5d38dff62e38bdf3c59e6050fb4be079e3dd6366
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-08-17 19:57:43 +0600

    location added

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
+import Location from "./Location.js";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
 
-export default Home
+  const defaultProps = {
+    center: {
+      lat: latitude,
+      lng: longitude,
+    },
+    zoom: 11,
+  };
+
+  // navigator.geolocation.getCurrentPosition(function (position) {
+  //   const lati = position.coords.latitude;
+  //   const long = position.coords.longitude;
+  //   setLatitude(lati);
+  //   setLongitude(long);
+  //   // return lati, long
+  //   console.log("Latitude is :", position.coords.latitude);
+  //   console.log("Longitude is :", position.coords.longitude);
+  // });
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lati = position.coords.latitude;
+      const long = position.coords.longitude;
+      setLatitude(lati);
+      setLongitude(long);
+      // return lati, long
+    });
+  }, []);
+  // console.log(latitude)
+  console.log("Latitude is :", latitude);
+  console.log("Longitude is :", longitude);
+
+  // https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&key=YOUR_API_KEY&signature=YOUR_SIGNATURE
+
+  return (
+    <div>
+      {/* <Location/> */}
+      <div className="text-center">
+        <p>Latitude: {latitude}</p>
+        <p>Longitude: {longitude}</p>
+        <Location/>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/Pages/Location.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, Fragment } from "react";
+import ReactDOM from "react-dom";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+
+
+const Location = () => {
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+
+  const center = {
+    lat: latitude,
+    lng: longitude,
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      const lati = position.coords.latitude;
+      const long = position.coords.longitude;
+      setLatitude(lati);
+      setLongitude(long);
+      // return lati, long
+    });
+  }, []);
+
+  return (
+    <div className="flex justify-center">
+      <LoadScript googleMapsApiKey='process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY'>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    </div>
+  );
+};
+
+export default Location;
