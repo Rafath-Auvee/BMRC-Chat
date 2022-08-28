817b3aff073ae90c9198afb223c35adc60c66320
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-08-28 15:51:14 +0600

    render deploy

--- src/Components/DataCollection/GeneralMemberReg.js
+++ src/Components/DataCollection/GeneralMemberReg.js
@@ -51,7 +51,7 @@ const GeneralMemberReg = () => {
             designation: "General Member",
             url: image,
           };
-          fetch(`https://bmrc-server-hokqr2h9x-rafath-bin-zafar-auvee.vercel.app/generalmember`, {
+          fetch(`https://bmrc.onrender.com/generalmember`, {
             method: "POST",
             headers: {
               "content-type": "application/json",
